9e77444b5ec36fb949a0d8465ccbed503eaf8679
Chris Skeen <chrisskeen2006@gmail.com>
2025-01-15 11:14:00 -0600

    ended a list

--- config/settings.py
+++ config/settings.py
@@ -131,6 +131,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = [
+  os.path.join(BASE_DIR, 'static'),
+]
 
 django_heroku.settings(locals())
